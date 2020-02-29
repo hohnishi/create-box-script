ccbebe1f4107bb49d0c38bd8c1e020bef3723777
hohnishi <Hajime.Ohnishi@gmail.com>
2020-02-29 13:18:50 +0900

    http server enable options

--- httpserver.go
+++ httpserver.go
@@ -1,11 +1,19 @@
+// httpserver -i 0.0.0.0 -p 8080 -d ./osfonf/
 package main
 
 import (
+  "flag"
+  "fmt"
   "net/http"
   "log"
 )
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./osconf/")))
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    i := flag.String("i", "0.0.0.0", "ip address/host name")
+    p := flag.String("p", "8080", "port number")
+    d := flag.String("d", "./osconf", "http root dir")
+    flag.Parse()
+    fmt.Println(*i,*p,*d)
+    http.Handle("/", http.FileServer(http.Dir(*d)))
+    log.Fatal(http.ListenAndServe(*i+":"+*p, nil))
 }
